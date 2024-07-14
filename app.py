4ca9fc86031d5bf2cc283dd3b64ed3cbbee9ae4b
George Gatheca <gatheca.george@gmail.com>
2024-07-14 14:55:48 +0100

    add redis to save data

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response, url_for
 from twilio.rest import Client
-import datetime
 import json
+import redis
 
+users = os.environ["ESCALATE_USERS"]
 app = Flask(__name__)
+r = redis.Redis(host='redis-alpine', port=6379, db=0)
+
+def save_data(key, value):
+    r.set(key, value)
+    print(f"Data saved: {key} -> {value}")
+
+def retrieve_data(key):
+    return r.get(key)
 
 @app.route("/")
 def index():
     version = os.environ["APP_VERSION"]
     return f"<h1>This is Twilio app version: {version}!</h1>"
+
+@app.route('/get/<key>', methods=['GET'])
+def get_value(key):
+  json_data = json.loads(retrieve_data(key))
+
+  say_text = f"<Say voice=\"woman\">Escalation for {json_data['alert_payload']['commonLabels']['alertname']} alert. There are {json_data['alert_payload']['numFiring']} alerts in firing state.</Say>"
+
+  index = 1
+  for alert in json_data['alert_payload']['alerts']:
+     say_text += f"<Pause length=\"1\"/><Say voice=\"woman\">Alert {index} summary: {alert['annotations']['summary']}. </Say>"
+     index += 1
+
+  xml_data = f'<Response>{say_text}</Response>'
+
+  return Response(xml_data, mimetype='text/xml')
 
 @app.route('/make-call', methods=['POST'])
 def make_call():
@@ -17,20 +41,20 @@ def make_call():
   auth_token = os.environ["TWILIO_AUTH_TOKEN"]
   client = Client(account_sid, auth_token)
 
-  current_datetime = datetime.datetime.now()
-  file_name = f"/data/file_{current_datetime.strftime('%Y-%m-%d-%H:%M:%S')}.txt"
-  f = open(file_name, "a")
-  f.write(json.dumps(request.json, indent=4, sort_keys=True))
-  f.close()
-  return request.json
+  json_data = request.json
+  id = json_data['alert_group_id']
+  formatted_json = json.dumps(json_data, indent=4, sort_keys=True)
+  save_data(id, formatted_json)
 
-  # call = client.calls.create(
-  #   url = "http://demo.twilio.com/docs/voice.xml",
-  #   to = "+351914077402",
-  #   from_ = os.environ["FROM_NUMBER"]
-  # )
+  say_url = url_for('get_value', key=id, _external=True)
 
-  # return (call.sid)
+  call = client.calls.create(
+    url = say_url,
+    to = users,
+    from_ = os.environ["FROM_NUMBER"]
+  )
+
+  return (call.sid)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5051, debug=True)
